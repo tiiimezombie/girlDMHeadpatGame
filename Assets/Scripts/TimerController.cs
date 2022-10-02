97d45423a2aa2e4d7d604fc9ce1a49f78b686cb5
tiiimezombie <tiiimezombie@gmail.com>
2022-10-02 18:38:38 -0230

    cleanup

---
+++ Assets/Scripts/TimerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] private TimerPanel[] _timerArray;
+    private bool _upgradeMode;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ToggleUpgradeMode()
+    {
+        _upgradeMode = !_upgradeMode;
+        foreach (var v in _timerArray)
+        {
+            v.SetView(_upgradeMode ? 2 : 1);
+        }
+    }
+}
---
+++ Assets/Scripts/TimerPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TimerPanel : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _titleText;
+    [SerializeField] private GameObject _viewOneHolderGO;
+    [SerializeField] private Image _progressBar;
+    [SerializeField] private Button _claimButton;
+    [SerializeField] private TextMeshProUGUI _claimButtonText;
+    [SerializeField] private GameObject _viewTwoHolderGO;
+    [SerializeField] private Button _xpUpgradeButton;
+    [SerializeField] private TextMeshProUGUI _xpUpgradeButtonText;
+    [SerializeField] private Button _moneyUpgradeButton;
+    [SerializeField] private TextMeshProUGUI _moneyUpgradeButtonText;
+
+    public void Setup(string name)
+    {
+        _viewOneHolderGO.SetActive(true);
+        _viewTwoHolderGO.SetActive(false);
+        _titleText.text = name;
+    }
+
+    public void DoUpdate()
+    {
+
+    }
+
+    public void SetView(int viewIndex)
+    {
+        _viewOneHolderGO.SetActive(false);
+        _viewTwoHolderGO.SetActive(false);
+
+        if (viewIndex == 2)
+        {
+            _viewTwoHolderGO.SetActive(true);
+        }
+        else
+        {
+            _viewOneHolderGO.SetActive(true);
+        }
+    }
+}
